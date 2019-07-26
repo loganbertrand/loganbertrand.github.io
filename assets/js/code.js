6446bdd2e93cf70852d3efee42d948bccb504bd4
Logan Bertrand <loganwbertrand@gmail.com>
2019-07-25 17:12:51 -0700

    additional functionality for better looks and flow

--- assets/js/code.js
+++
@@ -1,8 +0,0 @@
-//Javascript file for Portfolio
-
-$(document).ready(function(){
-    $( "btn-secondary" ).click(function( event ) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
-    });
-});
