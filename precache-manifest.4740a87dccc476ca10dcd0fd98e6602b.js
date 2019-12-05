66b94636b37697a8d0ae7462c0f622a5ad21976c
Logan Bertrand <loganwbertrand@gmail.com>
2019-12-05 15:30:38 -0800

    Updates

---
+++ precache-manifest.4740a87dccc476ca10dcd0fd98e6602b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37925e6c3c7b14a9e6fe4198f360c5f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1176220a64da662a94ab",
+    "url": "/static/css/main.7b987fd1.chunk.css"
+  },
+  {
+    "revision": "198c394bee49f808c738",
+    "url": "/static/js/2.19d116d2.chunk.js"
+  },
+  {
+    "revision": "1176220a64da662a94ab",
+    "url": "/static/js/main.4c7d5633.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "be43851e4a59d2952bdff6b966d9a756",
+    "url": "/static/media/firebase.be43851e.png"
+  },
+  {
+    "revision": "ec26ea62e4b3ad18c4337424c5368b6e",
+    "url": "/static/media/galaxy.ec26ea62.jpg"
+  },
+  {
+    "revision": "a2d1f52c9f82663aae75ff180006886b",
+    "url": "/static/media/garagesail.a2d1f52c.png"
+  },
+  {
+    "revision": "267657e869522c9e21eb501616a70d06",
+    "url": "/static/media/materialize.267657e8.png"
+  },
+  {
+    "revision": "df1d2359aae7f9411e1c768ccc998878",
+    "url": "/static/media/mongodb.df1d2359.png"
+  },
+  {
+    "revision": "5fc2eb804f1b1364d5e5549e07819e09",
+    "url": "/static/media/mysql.5fc2eb80.jpg"
+  },
+  {
+    "revision": "8452f08bf27201f085ffebc7bd95b705",
+    "url": "/static/media/pickupgames.8452f08b.png"
+  },
+  {
+    "revision": "c32674b9e2bfec13a078cf2638501a2a",
+    "url": "/static/media/profile-photo.c32674b9.jpg"
+  },
+  {
+    "revision": "3dfdddf9e1f16fd7b9ea3d9a8a6b810e",
+    "url": "/static/media/react-memory.3dfdddf9.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "82e44e638c0c23dffa235078375cf980",
+    "url": "/static/media/resume.82e44e63.pdf"
+  },
+  {
+    "revision": "26a74b7454844539f55d250414d43b90",
+    "url": "/static/media/winnable.26a74b74.png"
+  }
+]);
